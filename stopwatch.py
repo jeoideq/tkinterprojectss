95d212365d75e4d433a225c124c77dd029ff204d
jeoideq <hanssonj20@gmail.com>
2025-01-10 18:05:42 +0100

    Add files via upload

--- stopwatch.py
+++ stopwatch.py
@@ -1,4 +1,6 @@
 import tkinter 
+import time
+import tkinter.messagebox
 
 screen=tkinter.Tk()
 screen.geometry("300x200")
@@ -9,9 +11,41 @@ hour=tkinter.StringVar()
 minutes=tkinter.StringVar()
 
 sec=tkinter.StringVar()
-hour.set("1")
-minutes.set("0")
-sec.set("0")
+
+hour.set("00")
+minutes.set("00")
+sec.set("00")
+
+def start():
+    s=int(sec.get())
+    m=int(minutes.get())
+    h=int(hour.get())
+    
+    while not (h==0 and m==0 and s==0):
+        s-=1
+        if h>0 and m==0 and s==0:
+            h-=1
+            m=59
+            s=59
+
+        if s==0 and m>0:
+            m-=1
+            s+=60
+
+        if h==0 and m==0 and s==0:
+    
+            tkinter.messagebox.showinfo("time is up","time is up")
+            break
+        sec.set(s)
+        minutes.set(m)
+        hour.set(h)
+        screen.update()
+        time.sleep(1)
+
+
+    
+
+
 
 entry1=tkinter.Entry(screen, width=5, textvariable=hour)
 entry1.grid(row=1, column=1, padx=20)
@@ -22,7 +56,7 @@ entry2.grid(row=1, column=2, padx=20)
 entry3=tkinter.Entry(screen, width=5, textvariable=sec)
 entry3.grid(row=1, column=3, pady=50, padx=20)
 
-button=tkinter.Button(screen, text="Start")
+button=tkinter.Button(screen, text="Start", command=start)
 button.grid(row=2, column=2)
 
 screen.mainloop()
